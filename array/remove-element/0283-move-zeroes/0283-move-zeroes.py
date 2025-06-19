1b3df8498716ff92c35ea80620c7a8cf462007c8
LilyLiu945 <zhijiel9@uci.edu>
2025-06-19 14:12:29 -0700

    Update and rename 0283-move-zeroes/0283-move-zeroes.py to array/remove-element/0283-move-zeroes/0283-move-zeroes.py

--- array/remove-element/0283-move-zeroes/0283-move-zeroes.py
+++ array/remove-element/0283-move-zeroes/0283-move-zeroes.py
@@ -6,7 +6,10 @@ class Solution:
         slow = 0
         for fast in range(len(nums)):
             if nums[fast] != 0:
-                if slow != fast:
+                if slow != fast: # A zero has been skipped
                     nums[slow] = nums[fast]
                     nums[fast] = 0
-                slow += 1
+                slow += 1 # Valid element found so move the write pointer
+
+# - 'fast' scans through the array.
+# - 'slow' marks the position to place the next non-zero value.
