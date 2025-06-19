9135721cb202e96dcacc73d4ed43e39b8e7d7f4b
LilyLiu945 <zhijiel9@uci.edu>
2025-06-18 17:16:22 -0700

    Update 0034-find-first-and-last-position-of-element-in-sorted-array.py

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -9,9 +9,9 @@ class Solution:
                     left = mid + 1
                 else:
                     right = mid - 1
-            return left
-            
-# For left bound: right overstepped left → return left            
+            return left           
+# For left bound: right overstepped left → return left     
+        
         def findRight(nums: List[int], target: int) -> int:
             left, right = 0, len(nums) - 1
             while left <= right:
