3ab7c0e599336b4882e7d33b34852cadf2898433
LilyLiu945 <zhijiel9@uci.edu>
2025-07-17 18:05:14 -0700

    Update and rename 0503-next-greater-element-ii/0503-next-greater-element-ii.py to monotonic-stack/0503-next-greater-element-ii/0503-next-greater-element-ii.py

--- monotonic-stack/0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ monotonic-stack/0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -1,13 +1,19 @@
+# Time complexity: O(n)
+
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
         n = len(nums)
         res = [-1] * n
-        stack = []
-        for i in range(2 * n):
+        stack = [] # Stack stores "indices", not values
+        
+        for i in range(2 * n): # Simulate circular array
             cur = nums[i % n]
+            
             while stack and nums[stack[-1]] < cur:
                 prev = stack.pop()
                 res[prev] = cur
-            if i < n:
+                
+            if i < n: # Only push index in first pass
                 stack.append(i)
+                
         return res 
