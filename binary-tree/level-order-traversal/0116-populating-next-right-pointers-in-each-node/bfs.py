506d0c6c84a60c7a315fc56916dffad81771e063
LilyLiu945 <zhijiel9@uci.edu>
2025-07-21 15:22:01 -0700

    Update and rename 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py to binary-tree/level-order-traversal/0116-populating-next-right-pointers-in-each-node/bfs.py

--- binary-tree/level-order-traversal/0116-populating-next-right-pointers-in-each-node/bfs.py
+++ binary-tree/level-order-traversal/0116-populating-next-right-pointers-in-each-node/bfs.py
@@ -11,17 +11,20 @@ class Node:
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
         if not root:
-            return root
+            return None
         queue = deque([root])
         while queue:
-            prev = None
+            prev = None # Used to connect nodes at the same level
+            # Traverse all nodes at the current level
             for _ in range(len(queue)):
                 node = queue.popleft()
                 if prev:
-                    prev.next = node
-                prev = node
+                    prev.next = node # Connect the previous node to the current one
+                prev = node # Update prev to the current node
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
+             # At the end of the level, the last node's next is already None by default
+             # But you can explicitly write: if prev: prev.next = None
         return root
