24c7b5c6d40d762a8abfd5d009ace8f6cb097969
LilyLiu945 <zhijiel9@uci.edu>
2025-07-21 15:36:41 -0700

    Time: 42 ms (95.11%), Space: 19 MB (87.58%) - LeetHub

---
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root:
+            return None
+        leftmost = root # Start from the top level (root)
+        # Since it's a perfect binary tree, if leftmost has a left child, it must also have a right child
+        while leftmost.left:
+            head = leftmost # Pointer to traverse the current level
+            while head:
+                # Connect the left and right child of the same parent
+                head.left.next = head.right
+                # Connect right child to the next subtree's left child, if it exists
+                if head.next:
+                    head.right.next = head.next.left
+                # Move to the next node in the same level
+                head = head.next
+            # Move down to the next level (always the leftmost node)
+            leftmost = leftmost.left
+        return root
