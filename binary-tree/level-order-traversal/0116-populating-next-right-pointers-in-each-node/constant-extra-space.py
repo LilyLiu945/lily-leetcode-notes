16c9e7cf9460e9aaad60b087e027fdaa745641e4
LilyLiu945 <zhijiel9@uci.edu>
2025-07-21 15:39:34 -0700

    Update and rename 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py to binary-tree/level-order-traversal/0116-populating-next-right-pointers-in-each-node/constant-extra-space.py

--- binary-tree/level-order-traversal/0116-populating-next-right-pointers-in-each-node/constant-extra-space.py
+++ binary-tree/level-order-traversal/0116-populating-next-right-pointers-in-each-node/constant-extra-space.py
@@ -13,17 +13,22 @@ class Solution:
         if not root:
             return None
         leftmost = root # Start from the top level (root)
+        
         # Since it's a perfect binary tree, if leftmost has a left child, it must also have a right child
         while leftmost.left:
             head = leftmost # Pointer to traverse the current level
+            
             while head:
                 # Connect the left and right child of the same parent
                 head.left.next = head.right
+                
                 # Connect right child to the next subtree's left child, if it exists
                 if head.next:
                     head.right.next = head.next.left
+                    
                 # Move to the next node in the same level
                 head = head.next
+                
             # Move down to the next level (always the leftmost node)
             leftmost = leftmost.left
         return root
