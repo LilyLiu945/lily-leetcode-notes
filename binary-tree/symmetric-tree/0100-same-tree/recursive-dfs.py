cfc1517e22bae26f1d96028aa1a18c28aad3dc79
LilyLiu945 <zhijiel9@uci.edu>
2025-07-22 17:33:47 -0700

    Update and rename 0100-same-tree/0100-same-tree.py to binary-tree/symmetric-tree/0100-same-tree/recursive-dfs.py

--- binary-tree/symmetric-tree/0100-same-tree/recursive-dfs.py
+++ binary-tree/symmetric-tree/0100-same-tree/recursive-dfs.py
@@ -6,10 +6,15 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # Both nodes are null → this subtree is the same
         if not p and not q:
             return True
+            
+        # One is null, one is not → mismatch
         if not p or not q:
             return False
+            
+        # Check current node value and recurse on left and right children
         return (
             p.val == q.val
             and self.isSameTree(p.left, q.left)
