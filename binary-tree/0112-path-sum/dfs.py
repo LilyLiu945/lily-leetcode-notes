d57860069c6653658804a0438c1e374ace2e21bc
LilyLiu945 <zhijiel9@uci.edu>
2025-07-24 18:45:12 -0700

    Update and rename 0112-path-sum/0112-path-sum.py to binary-tree/0112-path-sum/dfs.py

--- binary-tree/0112-path-sum/dfs.py
+++ binary-tree/0112-path-sum/dfs.py
@@ -8,7 +8,11 @@ class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         if not root:
             return False
+            
+        # If it's a leaf node
         if not root.left and not root.right:
             return targetSum == root.val
+            
+        # Recursively check left and right
         return (self.hasPathSum(root.left, targetSum - root.val)
         or self.hasPathSum(root.right, targetSum - root.val))
