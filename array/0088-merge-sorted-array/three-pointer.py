534c9e9b069347dbb51f63eca065c5f66120644e
LilyLiu945 <zhijiel9@uci.edu>
2025-08-22 17:11:05 -0700

    Update and rename 0088-merge-sorted-array/0088-merge-sorted-array.py to array/0088-merge-sorted-array/three-pointer.py

--- array/0088-merge-sorted-array/three-pointer.py
+++ array/0088-merge-sorted-array/three-pointer.py
@@ -3,7 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        # three pointers: i -> nums1 end, j -> nums2 end, k -> final position
         i, j, k = m - 1, n - 1, m + n - 1
+
+        # compare and fill from the back
         while i >= 0 and j >= 0:
             if nums1[i] > nums2[j]:
                 nums1[k] = nums1[i]
@@ -12,6 +15,8 @@ class Solution:
                 nums1[k] = nums2[j]
                 j -= 1
             k -= 1
+            
+        # if nums2 still has elements, put them into nums1    
         while j >= 0:
             nums1[k] = nums2[j]
             j -= 1
