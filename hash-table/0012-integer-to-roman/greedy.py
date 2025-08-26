8c6973e04382fe15ac188016a3922778dd084da3
LilyLiu945 <zhijiel9@uci.edu>
2025-08-26 16:41:26 -0700

    Update and rename 0012-integer-to-roman/0012-integer-to-roman.py to hash-table/0012-integer-to-roman/greedy.py

--- hash-table/0012-integer-to-roman/greedy.py
+++ hash-table/0012-integer-to-roman/greedy.py
@@ -1,13 +1,14 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
         
+        # value-symbol pairs in descending order, include subtractive forms
         vals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
         syms = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
 
         res = []
         for v, s in zip(vals, syms):
-            cnt, num = divmod(num, v)
-            res.append(s * cnt)
+            cnt, num = divmod(num, v) # returns (num // v, num % v)
+            res.append(s * cnt) # append s cnt times (maybe 0)
             if num == 0:
                 break
             
