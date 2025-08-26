87eec9344b7ea6e1b23ba46f89d7f176bb4db06c
LilyLiu945 <zhijiel9@uci.edu>
2025-08-26 15:43:51 -0700

    Update sort.py

--- array/0274-h-index/sort.py
+++ array/0274-h-index/sort.py
@@ -1,8 +1,8 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        citations.sort(reverse=True)
+        citations.sort(reverse=True) # Descending order
         h = 0
-        for i, c in enumerate(citations, 1):  # i = 1..n
+        for i, c in enumerate(citations, 1):  # i start from 1
             if c >= i:
                 h = i
             else:
