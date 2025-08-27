942074b246e533ca0933c678e77c228b3ced607f
LilyLiu945 <zhijiel9@uci.edu>
2025-08-27 14:56:47 -0700

    Update and rename 0290-word-pattern.py to lengths-match.py

--- 0290-word-pattern/0290-word-pattern.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def wordPattern(self, pattern: str, s: str) -> bool:
-        words = s.split()
-        return (len(pattern) == len(words) and 
-        len(set(zip(pattern, words))) == len(set(pattern)) == len(set(words)))
---
+++ 0290-word-pattern/lengths-match.py
@@ -0,0 +1,8 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split()
+        return (len(pattern) == len(words) and 
+        len(set(zip(pattern, words))) == len(set(pattern)) == len(set(words)))
+        
+# Bijection check via set sizes: 
+# ensure lengths match, and unique (pattern, word) pairs == unique patterns == unique words.
