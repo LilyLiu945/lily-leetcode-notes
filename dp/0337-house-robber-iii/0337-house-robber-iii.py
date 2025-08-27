02796813be27ce4f97012f8abb48bef5b22f0636
LilyLiu945 <zhijiel9@uci.edu>
2025-08-26 18:11:09 -0700

    Update and rename 0337-house-robber-iii/0337-house-robber-iii.py to dp/0337-house-robber-iii/0337-house-robber-iii.py

--- dp/0337-house-robber-iii/0337-house-robber-iii.py
+++ dp/0337-house-robber-iii/0337-house-robber-iii.py
@@ -10,8 +10,8 @@ class Solution:
         def dfs(node: Optional[TreeNode]) -> tuple[int, int]:
             if not node:
                 return 0, 0
-            lt, ls = dfs(node.left)
-            rt, rs = dfs(node.right)
+            lt, ls = dfs(node.left) # lt: left-take, ls: left-skip
+            rt, rs = dfs(node.right) # rt: right-take, rs: right-skip
             take = node.val + ls + rs
             skip = max(lt, ls) + max(rt, rs)
             return take, skip
