5ce6a7e1c53ece0c9532455738c28ddcb915d914
LilyLiu945 <zhijiel9@uci.edu>
2025-08-26 17:50:28 -0700

    Time: 3 ms (88.31%), Space: 19.6 MB (85.92%) - LeetHub

---
+++ 0337-house-robber-iii/0337-house-robber-iii.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+
+        def dfs(node: Optional[TreeNode]) -> tuple[int, int]:
+            if not node:
+                return 0, 0
+            lt, ls = dfs(node.left)
+            rt, rs = dfs(node.right)
+            take = node.val + ls + rs
+            skip = max(lt, ls) + max(rt, rs)
+            return take, skip
+        
+        take, skip = dfs(root)
+        return max(take, skip)
