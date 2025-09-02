296cbfb053e60da7fe1ec3d775e695f710012e1e
LilyLiu945 <zhijiel9@uci.edu>
2025-09-02 14:49:54 -0700

    Update and rename 0014-longest-common-prefix/0014-longest-common-prefix.py to string/0014-longest-common-prefix/longitudinal-scan.py

--- string/0014-longest-common-prefix/longitudinal-scan.py
+++ string/0014-longest-common-prefix/longitudinal-scan.py
@@ -3,7 +3,7 @@ class Solution:
         if not strs:
             return ""
         res = []
-        for chars in zip(*strs):
+        for chars in zip(*strs): # *: argument unpacking
             if len(set(chars)) == 1:
                 res.append(chars[0])
             else:
