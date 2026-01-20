35c115136e2bff4726f4c1ee5cc0244a1c3c8800
LilyLiu945 <zhijiel9@uci.edu>
2026-01-19 17:40:26 -0800

    Add comments to merge intervals function

--- array/sorting/0056-merge-intervals/0056-merge-intervals.py
+++ array/sorting/0056-merge-intervals/0056-merge-intervals.py
@@ -1,12 +1,12 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort(key = lambda x: x[0])
+        intervals.sort(key = lambda x: x[0]) # sort first: O(nlogn)
         ans = []
 
         for s, e in intervals:
             if not ans or s > ans[-1][1]:
                 ans.append([s, e])
             else:
-                ans[-1][1] = max(ans[-1][1], e)
+                ans[-1][1] = max(ans[-1][1], e) # pay attention to overlapping boundaries
 
         return ans
