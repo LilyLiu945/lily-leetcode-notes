aa4760f6b01f01abdabcb202915d3f9cad65fd66
LilyLiu945 <zhijiel9@uci.edu>
2026-01-20 22:34:05 -0800

    Add greedy.py for stock buying and selling solution

--- dp/0122-best-time-to-buy-and-sell-stock-ii/greedy.py
+++ dp/0122-best-time-to-buy-and-sell-stock-ii/greedy.py
@@ -5,3 +5,7 @@ class Solution:
             if prices[i] > prices[i-1]:
                 profit += prices[i] - prices[i-1]
         return profit
+
+# This greedy solution is a special case of DP.
+# By summing all positive price differences, we achieve the same result
+# as the DP solution with states (hold, cash), but in a simpler form.
