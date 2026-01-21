f811d22e6212d93f2b262cab44d4444dd85c4709
LilyLiu945 <zhijiel9@uci.edu>
2026-01-21 00:04:20 -0800

    Clarify k transaction condition in maxProfit
    Add comment to clarify condition for k transactions.

--- dp/0188-best-time-to-buy-and-sell-stock-iv/state-compression.py
+++ dp/0188-best-time-to-buy-and-sell-stock-iv/state-compression.py
@@ -2,6 +2,7 @@ class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
         n = len(prices)
 
+        # When k is bigger than the maximum transactions, it can be reduced to 122.
         if k >= n // 2:
             profit = 0
             for i in range(1, n):
@@ -10,7 +11,7 @@ class Solution:
             return profit
 
         buy = [float('-inf')] * (k + 1)
-        sell = [0] * (k + 1)
+        sell = [0] * (k + 1)             # Retain sell[0] = 0
 
         for p in prices:
             for j in range(1, k + 1):
